f60b3ad98740e2ded1a4b8535c19810813f72e54
Diffuse <48339639+diffuse@users.noreply.github.com>
2022-06-15 23:46:03 -0400

    Move duplicate example code to one file

--- examples/multi-line-gradient.rs
+++ examples/multi-line-gradient.rs
@@ -1,25 +1,7 @@
 use disco::theme::Spectral;
 use disco::{ColorFormat, DiscoLogger, LoggerConfig, RecordFormat};
-use lipsum::lipsum;
 use log::LevelFilter;
-use rand::Rng;
-
-/// log n sample messages, weighted towards the default _ match arm
-fn log_sample_messages(n: usize) {
-    for _ in 0..n {
-        let n = rand::thread_rng().gen_range(0..50);
-        let msg = lipsum(n);
-
-        match rand::thread_rng().gen_range(0..15) {
-            0 => log::trace!("{}", msg),
-            1 => log::debug!("{}", msg),
-            2 => log::info!("{}", msg),
-            3 => log::warn!("{}", msg),
-            4 => log::error!("{}", msg),
-            _ => log::info!("{}", msg),
-        };
-    }
-}
+mod util;
 
 fn main() {
     // setup logger
@@ -32,5 +14,5 @@ fn main() {
     };
     DiscoLogger::new(config).init().unwrap();
 
-    log_sample_messages(1000);
+    util::log_sample_messages(1000);
 }
