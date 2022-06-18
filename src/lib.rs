2e102d9d7ab7472886872a867f0c913c53ec2c0a
diffuse <48339639+diffuse@users.noreply.github.com>
2022-06-18 16:27:39 -0400

    Change color_log args to msg and level, since other fields of record are unused

--- src/lib.rs
+++ src/lib.rs
@@ -229,23 +229,23 @@ impl DiscoLogger {
     /// Arguments
     ///
     /// * `msg` - message to color
-    /// * `record` - log record
-    fn color_log(&self, msg: String, record: &Record) -> String {
+    /// * `level` - level of this log line
+    fn color_log(&self, msg: String, level: Level) -> String {
         if self.config.color_format.is_none() {
             return msg;
         }
 
         let line = match self.config.color_format.as_ref().unwrap() {
-            ColorFormat::Solid => self.color_solid(record.level(), msg),
-            ColorFormat::InlineGradient => self.color_inline_gradient(record.level(), msg),
+            ColorFormat::Solid => self.color_solid(level, msg),
+            ColorFormat::InlineGradient => self.color_inline_gradient(level, msg),
             ColorFormat::MultiLineGradient => {
-                let l = self.color_multi_line_gradient(record.level(), msg);
+                let l = self.color_multi_line_gradient(level, msg);
 
                 // increment line counter for this level
                 self.lines_logged
                     .lock()
                     .unwrap()
-                    .entry(record.level())
+                    .entry(level)
                     .and_modify(|e| *e = e.wrapping_add(1))
                     .or_insert(0);
 
@@ -267,7 +267,7 @@ impl Log for DiscoLogger {
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
             let mut msg = self.format_record(record);
-            msg = self.color_log(msg, record);
+            msg = self.color_log(msg, record.level());
 
             match record.level() {
                 Level::Warn | Level::Error => {
@@ -619,6 +619,6 @@ mod tests {
 
         // input msg should not be altered by None color format
         let msg = "foo".to_string();
-        assert_eq!(logger.color_log(msg.clone(), &rec), msg);
+        assert_eq!(logger.color_log(msg.clone(), rec.level()), msg);
     }
 }
