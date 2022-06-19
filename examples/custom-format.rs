c3bcacce29cc02e4e91e8293ce437da2d16d5533
diffuse <48339639+diffuse@users.noreply.github.com>
2022-06-19 00:59:54 -0400

    Add example for custom record formatting

---
+++ examples/custom-format.rs
@@ -0,0 +1,26 @@
+use disco::{Config, DiscoLogger, RecordFormat};
+use log::{LevelFilter, Record};
+
+fn main() {
+    // create your own conversion from a
+    // log::Record to a String and wrap it
+    // in a Box to use with a DiscoLogger
+    let fmt_rec = Box::new(|r: &Record| -> String {
+        format!("[{}] {} - {}", r.target(), r.level(), r.args())
+    });
+
+    // setup logger
+    let config = Config {
+        level: LevelFilter::Trace,
+        record_format: RecordFormat::Custom(fmt_rec),
+        ..Default::default()
+    };
+    DiscoLogger::new(config).init().unwrap();
+
+    // log away!
+    log::trace!("foo");
+    log::debug!("bar");
+    log::info!("baz");
+    log::warn!("spam");
+    log::error!("eggs");
+}
