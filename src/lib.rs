845f43176fefc23a85c6f4714863546e26b90d41
diffuse <48339639+diffuse@users.noreply.github.com>
2022-10-10 16:29:09 -0400

    Refactor log sculpting into sculpt mod

--- src/lib.rs
+++ src/lib.rs
@@ -8,30 +8,20 @@
 
 use colored::Colorize;
 use log::{Level, LevelFilter, Log, Metadata, Record, SetLoggerError};
-use serde_json::json;
 use std::io;
 use std::io::Write;
 use std::sync::Mutex;
-use time::format_description::well_known::Iso8601;
-use time::OffsetDateTime;
 mod paint;
 use paint::LogPainter;
+mod sculpt;
+use sculpt::LogSculptor;
 
 pub mod color;
 pub mod theme;
 pub use color::{Rgb, RgbRange};
 pub use paint::ColorFormat;
+pub use sculpt::RecordFormat;
 use theme::Theme;
-
-/// Record formatting mode that determines how log records are structured
-pub enum RecordFormat {
-    /// JSON format
-    Json,
-    /// simple log format `<timestamp> [<target>] - <message>`
-    Simple,
-    /// custom record formatter provided by client code
-    Custom(Box<dyn Sync + Send + Fn(&Record) -> String>),
-}
 
 /// Main configuration for a `DiscoLogger`
 pub struct Config {
@@ -66,8 +56,6 @@ impl Default for Config {
 
 /// Implements log::Log
 pub struct DiscoLogger {
-    /// Record format for logs
-    record_format: RecordFormat,
     /// Level-based filter for logs
     level_filter: LevelFilter,
     /// switch for enabling log splitting to `stderr`
@@ -78,6 +66,8 @@ pub struct DiscoLogger {
     use_stderr: bool,
     /// painter for logs
     log_painter: LogPainter,
+    // sculptor for logs
+    log_sculptor: LogSculptor,
     /// guard against interleaving from simultaneous writes to stdout + stderr
     write_mtx: Mutex<()>,
     /// handle to stdout
@@ -99,7 +89,7 @@ impl DiscoLogger {
     /// * `config` - configuration for this logger
     pub fn with_config(config: Config) -> DiscoLogger {
         DiscoLogger {
-            record_format: config.record_format,
+            log_sculptor: LogSculptor::new(config.record_format),
             level_filter: config.level,
             log_painter: LogPainter::new(config.theme, config.color_format),
             use_stderr: config.use_stderr,
@@ -112,37 +102,6 @@ impl DiscoLogger {
     /// Initialize this logger
     pub fn init(self) -> Result<(), SetLoggerError> {
         log::set_boxed_logger(Box::new(self)).map(|()| log::set_max_level(LevelFilter::Trace))
-    }
-
-    /// Convert a log record into a formatted string, based on the current logger configuration
-    ///
-    /// # Arguments
-    ///
-    /// * `record` - the log record to format
-    fn format_record(&self, record: &Record) -> String {
-        let now = OffsetDateTime::now_utc()
-            .format(&Iso8601::DEFAULT)
-            .expect("Failed to format time as ISO 8601");
-
-        match &self.record_format {
-            RecordFormat::Json => json!({
-                "time": now,
-                "level": record.level(),
-                "target": record.target(),
-                "message": record.args(),
-            })
-            .to_string(),
-            RecordFormat::Simple => {
-                format!(
-                    "{} [{}] {} - {}",
-                    now,
-                    record.target(),
-                    record.level(),
-                    record.args()
-                )
-            }
-            RecordFormat::Custom(f) => f(record),
-        }
     }
 }
 
@@ -159,7 +118,7 @@ impl Log for DiscoLogger {
     /// * `record` - the record to log
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let mut msg = self.format_record(record);
+            let mut msg = self.log_sculptor.sculpt(record);
             msg = self.log_painter.paint(msg, record.level());
 
             // stdout and stderr already have their own locks, but
@@ -191,69 +150,6 @@ impl Log for DiscoLogger {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn format_record_presets_return_non_empty() {
-        for fmt in vec![RecordFormat::Json, RecordFormat::Simple] {
-            let config = Config {
-                record_format: fmt,
-                ..Default::default()
-            };
-            let logger = DiscoLogger::with_config(config);
-
-            // create normal test record
-            let rec = Record::builder()
-                .args(format_args!("foo"))
-                .level(Level::Info)
-                .target("test")
-                .build();
-
-            assert!(!logger.format_record(&rec).is_empty());
-
-            // create record with empty args and target
-            let rec = Record::builder()
-                .args(format_args!(""))
-                .level(Level::Info)
-                .target("")
-                .build();
-
-            // record should still give non-empty log lines
-            assert!(!logger.format_record(&rec).is_empty());
-        }
-    }
-
-    #[test]
-    fn format_record_custom_formats_correctly() {
-        let test_cases = vec![
-            (RecordFormat::Custom(Box::new(|_| "".to_string())), ""),
-            (
-                RecordFormat::Custom(Box::new(|r| format!("{} {}", r.level(), r.args()))),
-                "INFO foo",
-            ),
-            (
-                RecordFormat::Custom(Box::new(|r| {
-                    format!("{} [{}] {}", r.level(), r.target(), r.args())
-                })),
-                "INFO [test] foo",
-            ),
-        ];
-
-        let rec = Record::builder()
-            .args(format_args!("foo"))
-            .level(Level::Info)
-            .target("test")
-            .build();
-
-        for (fmt, expected) in test_cases {
-            let config = Config {
-                record_format: fmt,
-                ..Default::default()
-            };
-            let logger = DiscoLogger::with_config(config);
-
-            assert_eq!(logger.format_record(&rec), expected);
-        }
-    }
 
     #[test]
     fn enabled_filters_levels() {
