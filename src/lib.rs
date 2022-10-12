8a4c6fbb0f92404845c06234135c5e09d5d19e29
Diffuse <48339639+diffuse@users.noreply.github.com>
2022-10-11 20:39:28 -0400

    Move config related code to config mod

--- src/lib.rs
+++ src/lib.rs
@@ -21,38 +21,9 @@ pub mod theme;
 pub use color::{Rgb, RgbRange};
 pub use paint::ColorFormat;
 pub use sculpt::RecordFormat;
+pub mod config;
+pub use config::Config;
 use theme::Theme;
-
-/// Main configuration for a `DiscoLogger`
-pub struct Config {
-    /// log level filter (logs below this severity will be ignored)
-    pub level: LevelFilter,
-    /// record formatting mode (determines how log records are structurally formatted)
-    pub record_format: RecordFormat,
-    /// color formatting mode (determines how log records are colored)
-    pub color_format: Option<ColorFormat>,
-    /// color theme (determines the color palette used to color log records)
-    pub theme: Box<dyn Theme>,
-    /// switch for enabling log splitting to `stderr`
-    ///
-    /// - `true`: log `trace` - `info` levels to `stdout` and `warn` - `error` levels to `stderr`
-    ///
-    /// - `false`: log all levels to `stdout`
-    pub use_stderr: bool,
-}
-
-impl Default for Config {
-    /// Return a `Config` with default values
-    fn default() -> Config {
-        Config {
-            level: LevelFilter::Info,
-            record_format: RecordFormat::Simple,
-            color_format: Some(ColorFormat::Solid),
-            theme: Box::new(theme::Simple {}),
-            use_stderr: true,
-        }
-    }
-}
 
 /// Implements log::Log
 pub struct DiscoLogger {
